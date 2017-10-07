36ed9f263f8c97161d0264392030bbe32c27a7e9
Zhang Sheng <zshe129@aucklanduni.ac.nz>
2017-10-07 21:56:20 +1300

    V0.1

---
+++ config/web.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'web_status' => '0',
+  'web_site' => '  博客  ',
+  'web_count' => '百度统计',
+  'seo_title' => '   个人博客   ',
+  'keywords' => '   个人项目,个人博客,个人项目练习   ',
+  'description' => 'phper,程序员，php工程师',
+  'copyright' => 'Design by KwongYanChan <a href="http://www.kwongyanchan.pro/" target="_blank">http://www.kwongyanchan.pro</a>',
+);
